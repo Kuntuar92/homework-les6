817a25292f378108d1cb30285fa29e4522679e77
Kuntuar <kuntuar92@mail.ru>
2022-11-29 02:54:40 +0600

    lesson 7

---
+++ src/test/java/homework/ls7/EotinishTest.java
@@ -0,0 +1,30 @@
+package homework.ls7;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class EotinishTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = ("https://eotinish.kz/");
+    }
+
+
+    @ParameterizedTest(name = "Поиск кнопки {1} на локали {0}")
+    @CsvSource(value = {
+            "kk, Менің өтініштерім",
+            "ru, Мои обращения"
+    })
+    void checkTextInHeaderLocal(String locale, String elemHeader) {
+        open(locale);
+        $(by("data-test-id", "myAppealsBtn")).$(byText(elemHeader)).shouldBe(visible);
+    }
+}
---
+++ src/test/java/homework/ls7/KolesaWebTest.java
@@ -0,0 +1,50 @@
+package homework.ls7;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class KolesaWebTest {
+
+    static Stream<Arguments> kolesaTest() {
+
+        return Stream.of(
+                Arguments.of("Легковые", List.of(
+                        "Год выпуска",
+                        "Цена",
+                        "Купить в кредит",
+                        "Первоначальный взнос",
+                        "Ежемесячный платеж")),
+
+                Arguments.of("Мототехника",  List.of(
+                        "Год выпуска",
+                        "Цена"))
+        );
+    }
+    @BeforeEach
+    void setUp() {
+        Configuration.holdBrowserOpen = true;
+        open("https://kolesa.kz");
+    }
+
+    @MethodSource
+    @ParameterizedTest(name = "Найти в фильтре {0} поля {1}")
+    @Tag("MEDIUM")
+
+    void kolesaTest(String searchForm, List <String> filters) {
+        $(".action-list").$(byText(searchForm)).click();
+        //$(by("data-id", "2")).click();
+        $$(".secondary>.element-group").filter(visible).shouldHave(CollectionCondition.texts(filters));
+
+    }
+}
